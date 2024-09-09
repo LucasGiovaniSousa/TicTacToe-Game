036a10d45ef1dccb8573ac03762b67b374275091
LucasGiovaniSousa <167085822+LucasGiovaniSousa@users.noreply.github.com>
2024-09-09 23:46:06 +0100

    Up

---
+++ TicTacToe Game/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace TicTacToe_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            CPUTimer = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Historic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(20, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(107, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Player Wins:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Historic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(357, 29);
+            label2.Name = "label2";
+            label2.Size = new Size(91, 21);
+            label2.TabIndex = 1;
+            label2.Text = "CPU Wins:";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.AppWorkspace;
+            button1.Location = new Point(61, 88);
+            button1.Name = "button1";
+            button1.Size = new Size(114, 99);
+            button1.TabIndex = 2;
+            button1.Text = "GGG";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += PlayerClickButton;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.AppWorkspace;
+            button2.Location = new Point(181, 88);
+            button2.Name = "button2";
+            button2.Size = new Size(114, 99);
+            button2.TabIndex = 3;
+            button2.Text = "GGG";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += PlayerClickButton;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.AppWorkspace;
+            button3.Location = new Point(301, 88);
+            button3.Name = "button3";
+            button3.Size = new Size(114, 99);
+            button3.TabIndex = 4;
+            button3.Text = "GGG";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += PlayerClickButton;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.AppWorkspace;
+            button4.Location = new Point(301, 193);
+            button4.Name = "button4";
+            button4.Size = new Size(114, 99);
+            button4.TabIndex = 7;
+            button4.Text = "GGG";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += PlayerClickButton;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.AppWorkspace;
+            button5.Location = new Point(181, 193);
+            button5.Name = "button5";
+            button5.Size = new Size(114, 99);
+            button5.TabIndex = 6;
+            button5.Text = "GGG";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += PlayerClickButton;
+            // 
+            // button6
+            // 
+            button6.BackColor = SystemColors.AppWorkspace;
+            button6.Location = new Point(61, 193);
+            button6.Name = "button6";
+            button6.Size = new Size(114, 99);
+            button6.TabIndex = 5;
+            button6.Text = "GGG";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += PlayerClickButton;
+            // 
+            // button7
+            // 
+            button7.BackColor = SystemColors.AppWorkspace;
+            button7.Location = new Point(301, 298);
+            button7.Name = "button7";
+            button7.Size = new Size(114, 99);
+            button7.TabIndex = 10;
+            button7.Text = "GGG";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += PlayerClickButton;
+            // 
+            // button8
+            // 
+            button8.BackColor = SystemColors.AppWorkspace;
+            button8.Location = new Point(181, 298);
+            button8.Name = "button8";
+            button8.Size = new Size(114, 99);
+            button8.TabIndex = 9;
+            button8.Text = "GGG";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += PlayerClickButton;
+            // 
+            // button9
+            // 
+            button9.BackColor = SystemColors.AppWorkspace;
+            button9.Location = new Point(61, 298);
+            button9.Name = "button9";
+            button9.Size = new Size(114, 99);
+            button9.TabIndex = 8;
+            button9.Text = "GGG";
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += PlayerClickButton;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(143, 414);
+            button10.Name = "button10";
+            button10.Size = new Size(189, 35);
+            button10.TabIndex = 11;
+            button10.Text = "Restart Game";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += RestartGame;
+            // 
+            // CPUTimer
+            // 
+            CPUTimer.Interval = 1000;
+            CPUTimer.Tick += CPUmove;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(484, 461);
+            Controls.Add(button10);
+            Controls.Add(button7);
+            Controls.Add(button8);
+            Controls.Add(button9);
+            Controls.Add(button4);
+            Controls.Add(button5);
+            Controls.Add(button6);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Tic Tac Toe Game Against CPU";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private System.Windows.Forms.Timer CPUTimer;
+    }
+}
